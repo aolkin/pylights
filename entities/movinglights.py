0862199e4f901844a285e4d897f2ee4b5b82245b
Aaron Olkin <aaron@olkin.net>
2013-08-22 16:03:00 -0400

    Committing some other stuff in case sphinx blows up

---
+++ entities/movinglights.py
@@ -0,0 +1,9 @@
+from . import DictEntity
+
+from keys import names
+
+from fields import *
+
+names.addseq("<setup>15<enter>2<enter>","fixturepatch","movinglightpatch")
+names.addseq("<softkey8>","fixture","movinglight")
+
--- tests.py
+++ tests.py
@@ -1,8 +1,11 @@
+from __future__ import print_function
 """Uses easygui to present simple choices to test the sending of various entities"""
 
 import easygui
 from fields import *
-import sender, keys
+#import sender, keys
 
 def send_str(string):
-    sender.send(keys.parse_str(string))
+    string = keys.parse_str(string)
+    print("Sending {}".format(string))
+    sender.send(string)

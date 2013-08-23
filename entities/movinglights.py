96a1bf130ca62f6e4f472b772a3ca1a8a1555eb4
Aaron Olkin <aaron@olkin.net>
2013-08-23 04:55:50 -0400

    Removed actual code from github pages branch

--- entities/movinglights.py
+++
@@ -1,9 +0,0 @@
-from . import DictEntity
-
-from keys import names
-
-from fields import *
-
-names.addseq("<setup>15<enter>2<enter>","fixturepatch","movinglightpatch")
-names.addseq("<softkey8>","fixture","movinglight")
-
